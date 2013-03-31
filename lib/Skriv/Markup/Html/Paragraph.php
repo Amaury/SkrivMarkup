d1fe17fccc305144e01d7704eeaca0bb4f2a0c62
Amaury Bouchard <amaury@amaury.net>
2013-03-31 14:16:07 +0200

    Support of inline extensions at the beginning of a paragraph.

--- lib/Skriv/Markup/Html/Paragraph.php
+++ lib/Skriv/Markup/Html/Paragraph.php
@@ -2,40 +2,38 @@
 
 namespace Skriv\Markup\Html;
 
-/**
- * traite les signes de type paragraphe
- */
+/** Management of paragraph blocks of text. */
 class Paragraph extends \WikiRenderer\Block {
 	public $type = 'p';
 	protected $_openTag = '<p>';
 	protected $_closeTag = '</p>';
-	// attribut utilisé pour gérer les retours charriots dans les paragraphes
+	/** Attribute used to manage carriage-returns inside paragraphs. */
 	private $_firstLine = true;
 
 	/**
-	 * Détection des paragraphes
-	 * @param	string	$string		Ligne de texte servant pour la détection.
-	 * @param	bool	$inBlock	(optional) True is the parser is already in the block.
-	 * @return	bool	True si c'est un paragraphe.
+	 * Detection of paragraphs.
+	 * @param	string	$string		Line of text.
+	 * @param	bool	$inBlock	(optional) True if the parser is already in the block.
+	 * @return	bool	True if it's a paragraph.
 	 */
 	public function detect($string, $inBlock=false) {
 		if (empty($string))
 			return (false);
 		if (!preg_match("/^\s*\*{2}.*\*{2}\s*.*$/", $string) &&
 		    !preg_match("/^\s*#{2}.*#{2}\s*.*$/", $string) &&
-		    preg_match("/^\s*[\*#\-\!\| \t>;<=].*/", $string))
+		    preg_match("/^\s*[\*#\-\!\| \t>;=].*/", $string))
 			return (false);
 		$this->_detectMatch = array($string, $string);
 		return (true);
 	}
 	/**
-	 * Traitement du texte à l'intérieur d'un paragraphe.
-	 * @param	string	$string	Texte à traiter.
-	 * @return	string	Texte traité.
+	 * Rendering of the text insed a paragraph.
+	 * @param	string	$string	Text to render.
+	 * @return	string	Rendered result.
 	 */
 	protected function _renderInlineTag($string) {
 		$string = $this->engine->inlineParser->parse($string);
-		// gestion des retours-charriot dans les paragraphes
+		// handling of carriage-returns inside paragraphs
 		$string = (!$this->_firstLine) ? "<br />$string" : $string;
 		$this->_firstLine = false;
 		return ($string);
